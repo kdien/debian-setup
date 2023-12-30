fa441f6e16054834ca588fae2dae3768b53d6928
Khoa Dien <khoa.dien@hotmail.com>
2023-12-30 16:23:14 -0400

    Install `node` with webi instead of nodesource

--- setup.sh
+++ setup.sh
@@ -56,12 +56,6 @@ rm -f nf-symbols.tar.xz
 
 # Enable additional repos
 sudo add-apt-repository -y universe multiverse restricted
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-sudo tee /etc/apt/sources.list.d/nodesource.list <<EOF
-deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main
-EOF
 sudo apt update
 
 # Remove bloat
